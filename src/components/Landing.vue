0ded0d73fb17cc50f79c674cd31e7d23c83f545b
CJ R <cj@null.computer>
2017-04-24 10:09:37 -0600

    :sparkles: Add login button

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="landing">
     <h2>Admissions Portfolio</h2>
-    <v-btn class="orange darken-1" v-if="!token && !portfolio_id" v-on:click.native="apply">Apply Now</v-btn></a>
+    <v-btn class="orange darken-1" v-if="!token && !portfolio_id" :router="true" :to="{ name: 'login' }">Login</v-btn></a>
     <v-btn primary dark v-if="!token && portfolio_id" :router="true" :to="{ name: 'portfolio', params: { portfolio_id: portfolio_id }}">Go To Portfolio</v-btn>
     <v-btn primary dark v-if="token" :router="true" :to="{ name: 'admin' }" class="orange darken-1">View Applicants</v-btn>
   </div>
@@ -16,11 +16,6 @@ export default {
     },
     token() {
       return localStorage.token;
-    },
-  },
-  methods: {
-    apply() {
-      window.location = 'https://new.galvanize.com/web-development/application';
     },
   },
 };
